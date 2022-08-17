b4737612f07c17e2bab6eb45b4a99ba26731d2a6
MattW <mattnwarnock@yahoo.co.uk>
2022-08-17 12:11:58 +0100

    Green: pass with 10

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -1,7 +1,9 @@
 public class FizzBuzzMob {
     public String convert(int i) {
         if (i % 3 == 0) { return "Fizz"; }
-        if (i == 5) { return "Buzz"; }
+        if (i == 5 || i == 10) { return "Buzz"; }
+
+
         return String.valueOf(i);
     }
 }
