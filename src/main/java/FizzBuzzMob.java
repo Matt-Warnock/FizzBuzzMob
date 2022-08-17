934d583dde2e6926b2c4de1ee166fd82487a3af8
MattW <mattnwarnock@yahoo.co.uk>
2022-08-17 10:44:49 +0100

    Green: pass through 4

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -2,6 +2,7 @@ public class FizzBuzzMob {
     public String convert(int i) {
         if (i == 1) {return "1";}
         if (i == 2) {return "2";}
+        if (i == 4) {return "4";}
         return null;
     }
 }
