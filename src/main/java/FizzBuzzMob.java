bd2aa4ac9e79cf084c2376b29a606d955af71554
MattW <mattnwarnock@yahoo.co.uk>
2022-08-17 11:19:34 +0100

    Green: Fizz with 9"

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -2,6 +2,8 @@ public class FizzBuzzMob {
     public String convert(int i) {
         if (i == 3) {return "Fizz";}
         if (i == 6) {return "Fizz";}
+        if (i == 9) {return "Fizz";}
+
         return String.valueOf(i);
     }
 }
