1f03b5bf27a3e0b838d034206c5a1643ebf3c5bd
MattW <mattnwarnock@yahoo.co.uk>
2022-08-17 11:49:24 +0100

    Green: divisable by 3

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -1,8 +1,6 @@
 public class FizzBuzzMob {
     public String convert(int i) {
-        if (i == 3) {return "Fizz";}
-        if (i == 6) {return "Fizz";}
-        if (i == 9) {return "Fizz";}
+        if (i % 3 == 0) { return "Fizz"; }
 
         return String.valueOf(i);
     }
