764b23470e3e089ea1a302f346e423c41a7455f2
timcastillogill <timgillviola@gmail.com>
2022-08-17 12:14:37 +0100

    Green: Given 20 returning Buzz

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -1,7 +1,7 @@
 public class FizzBuzzMob {
     public String convert(int i) {
         if (i % 3 == 0) { return "Fizz"; }
-        if (i == 5 || i == 10) { return "Buzz"; }
+        if (i == 5 || i == 10 || i == 20) { return "Buzz"; }
 
 
         return String.valueOf(i);
