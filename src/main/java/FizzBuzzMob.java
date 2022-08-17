122b63e1449f90074a0552a60b12df30bca50e0a
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-17 11:01:09 +0100

    Green: passes through 6

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -1,6 +1,7 @@
 public class FizzBuzzMob {
     public String convert(int i) {
         if (i == 3) {return "Fizz";}
+        if (i == 6) {return "Fizz";}
         return String.valueOf(i);
     }
 }
