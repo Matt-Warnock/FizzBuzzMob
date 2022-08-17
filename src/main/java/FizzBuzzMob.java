39fe8be2100fc352705ff78627a87d44d8ed02b2
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-17 12:20:17 +0100

    Green: passes through 15

--- src/main/java/FizzBuzzMob.java
+++ src/main/java/FizzBuzzMob.java
@@ -1,8 +1,8 @@
 public class FizzBuzzMob {
     public String convert(int i) {
+        if (i == 15) { return "Fizz Buzz";}
         if (i % 3 == 0) { return "Fizz"; }
         if (i % 5 == 0) { return "Buzz"; }
-
 
         return String.valueOf(i);
     }
