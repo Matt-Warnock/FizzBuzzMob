aeafe94528115574ffe096d3e15e9de10f2945ab
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-17 11:12:02 +0100

    Refactor: new instance of class within tests

--- src/test/java/FizzBuzzMobTest.java
+++ src/test/java/FizzBuzzMobTest.java
@@ -1,30 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzMobTest {
+    private FizzBuzzMob fizzBuzzMob;
+    @BeforeEach
+    public void setUp() {
+        fizzBuzzMob = new FizzBuzzMob();
+    }
+
     @Test
     void convertsOne() {
-        assertEquals("1", new FizzBuzzMob().convert(1));
+        assertEquals("1", fizzBuzzMob.convert(1));
     }
 
     @Test
     void convertsTwo() {
-        assertEquals("2", new FizzBuzzMob().convert(2));
+        assertEquals("2", fizzBuzzMob.convert(2));
     }
 
     @Test
     void convertsFour() {
-        assertEquals("4", new FizzBuzzMob().convert(4));
+        assertEquals("4", fizzBuzzMob.convert(4));
     }
     @Test
     void convertsThree() {
-        assertEquals("Fizz", new FizzBuzzMob().convert(3));
+        assertEquals("Fizz", fizzBuzzMob.convert(3));
     }
 
     @Test
     void convertsSix() {
-        assertEquals("Fizz", new FizzBuzzMob().convert(6));
+        assertEquals("Fizz", fizzBuzzMob.convert(6));
     }
 
 }
